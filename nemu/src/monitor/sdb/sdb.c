fbb7121a7aefe757fa8f3214c3a6dbff3c81f338
tracer-ics2023 <tracer@njuics.org>
2023-10-02 15:28:22 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:28:22 up  6:44,  2 users,  load average: 0.20, 0.41, 0.32

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -70,6 +70,8 @@ static int cmd_si(char *args) {
 static int cmd_info(char *args) {
   char *token;
   token = strtok(args, " ");
+  if (token == NULL)
+    return 0;
   if (strcmp(token, "r") == 0)
     isa_reg_display();
   return 0;
