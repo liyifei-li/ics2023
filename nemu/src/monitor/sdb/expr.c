f6d3e6f7e753bde9adec1fc573c26c511f13ef61
tracer-ics2023 <tracer@njuics.org>
2023-10-04 13:02:49 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:02:49 up  4:47,  5 users,  load average: 0.69, 0.46, 0.32

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -138,7 +138,7 @@ static bool make_token(char *e) {
     }
     if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
-      printf("%d", e[position]);
+      printf("%d %d", e[position], '\0');
       assert(e[position] != '\0');
       return false;
     }
