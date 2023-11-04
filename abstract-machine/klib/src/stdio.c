43ae819bf2d4675f0173d5fd3167e2b19a59f039
tracer-ics2023 <tracer@njuics.org>
2023-11-04 22:29:16 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:29:16 up 13:59, 13 users,  load average: 0.84, 0.59, 0.48

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,8 +13,59 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+int sprintf(char *str, const char *format, ...) {
+  size_t i = 0, j = 0;
+  int dptr;
+  char *sptr;
+  char minint[15] = {'-', '2', '1', '4', '7', '4', '8', '3', '6', '4', '8'};
+  va_list ap;
+  va_start(ap, format);
+  while (format[j] != '\0') {
+    if (format[j] != '%') {
+      str[i] = format[j];
+      i++;
+      j++;
+    }
+    else {
+      switch(format[j + 1]) {
+        case 's':
+          sptr = va_arg(ap, char*);
+          strcpy(str + i, sptr);
+          i += strlen(sptr);
+          j += 2;
+          break;
+        case 'd':
+          dptr = *va_arg(ap, int*);
+          if (dptr == 0) {
+            str[i++] = '0';
+          }
+          else if (dptr == 0x80000000) {
+            strcpy(str + i, minint);
+            i += 11;
+          }
+          else {
+            if (dptr < 0) {
+              dptr = -dptr;
+              str[i++] = '-';
+            }
+            else {
+              while (dptr) {
+                str[i++] = dptr % 10 + '0';
+                dptr /= 10;
+              }
+            }
+          }
+          j += 2;
+          break;
+        default:
+          panic("Not implemented");
+          break;
+      }
+    }
+  }
+  str[i] = '\0';
+  va_end(ap);
+  return i;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
