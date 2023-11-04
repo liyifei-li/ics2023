3f2df746db17e0d60f821080fd9bda5c10eed74c
tracer-ics2023 <tracer@njuics.org>
2023-11-04 19:44:23 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:44:23 up 11:14, 13 users,  load average: 0.38, 0.40, 0.36

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -74,14 +74,14 @@ void *memmove(void *dst, const void *src, size_t n) {
   if (dst > src) {
     i = 0;
     while (i < n) {
-      *(uint8_t *)(dst + i) = *(uint8_t *)src + i;
+      *(uint8_t *)(dst + i) = *(uint8_t *)(src + i);
       i++;
     }
   }
   else {
     i = n - 1;
     while (i > 0) {
-      *(uint8_t *)(dst + i) = *(uint8_t *)src + i;
+      *(uint8_t *)(dst + i) = *(uint8_t *)(src + i);
       i--;
     }
     *(uint8_t *)dst = *(uint8_t *)src;
