f2c73b7c8ee3d37eda22d8c1b6a40be12361458a
tracer-ics2023 <tracer@njuics.org>
2023-11-04 17:06:37 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:06:37 up  8:36, 13 users,  load average: 0.31, 0.30, 0.29

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -46,6 +46,7 @@ int strcmp(const char *s1, const char *s2) {
   while (1) {
     if (s1[i] - s2[i] != 0) return s1[i] - s2[i];
     if (s1[i] == '\0' && s2[i] == '\0') return 0;
+    i++;
   }
 }
 
