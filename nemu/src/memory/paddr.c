29100b71c230361347e3052e845a5dac93b82e72
tracer-ics2023 <tracer@njuics.org>
2023-11-06 11:49:43 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:49:43 up  3:08,  1 user,  load average: 0.17, 0.24, 0.28

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -29,7 +29,7 @@ paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 static word_t pmem_read(paddr_t addr, int len) {
   word_t ret = host_read(guest_to_host(addr), len);
-  IFDEF(CONFIG_MTRACE, printf("read memory address: 0x%09x, length: %d\n", addr, len));
+  IFDEF(CONFIG_MTRACE, printf("read memory address: 0x%08x, length: %d\n", addr, len));
   return ret;
 }
 
