8bae1446c733d590c28acf0dd1326cee4ab5dc9f
tracer-ics2023 <tracer@njuics.org>
2023-11-06 10:37:27 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:37:27 up  1:55,  1 user,  load average: 0.14, 0.18, 0.18

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -90,11 +90,10 @@ static void exec_once(Decode *s, vaddr_t pc) {
   p[0] = '\0'; // the upstream llvm does not support loongarch32r
 #endif
   
-  pp = iringpos[iringbuf] + 48;
-  for (i = ilen - 1; i >= 0; i++) {
+  //pp = iringpos[iringbuf] + 48;
+  for (i = ilen - 1; i >= 0; i--) {
     pp += snprintf(pp, 4, " %02x", inst[i]);
   }
-  //pp = iringpos[iringbuf] + 48;
 
 #endif
 }
