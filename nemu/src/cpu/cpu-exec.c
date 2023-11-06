7fafb0d0420ea1edf76a02e16737a94f64e39fd4
tracer-ics2023 <tracer@njuics.org>
2023-11-06 11:11:40 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:11:40 up  2:30,  1 user,  load average: 0.19, 0.27, 0.25

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -82,8 +82,10 @@ static void exec_once(Decode *s, vaddr_t pc) {
   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
+/*
   disassemble(pp, iringbuf[iringpos] + sizeof(iringbuf[iringpos]) - pp,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
+*/
 #else
   p[0] = '\0'; // the upstream llvm does not support loongarch32r
 #endif
