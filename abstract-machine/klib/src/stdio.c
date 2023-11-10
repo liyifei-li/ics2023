e267e5fcc23cd86bf9d4c05b3144f7a6bac548f5
tracer-ics2023 <tracer@njuics.org>
2023-11-10 20:48:24 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:48:24 up 11:49,  5 users,  load average: 0.52, 0.40, 0.32

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,6 +5,67 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+//general printf
+int gprintf(unsigned char type, char *str, const char *fmt, va_list ap) {
+  size_t i = 0, j = 0, k;
+  int dptr;
+  char *sptr;
+  char minint[15] = "2147483638";
+  char dtos[15] = {0};
+  bool isneg = 0;
+  while (fmt[j] != '\0') {
+    if (fmt[j] != '%') {
+      str[i] = fmt[j];
+      i++;
+      j++;
+    }
+    else {
+      switch(fmt[j + 1]) {
+        case 's':
+          sptr = va_arg(ap, char*);
+          strcpy(str + i, sptr);
+          i += strlen(sptr);
+          j += 2;
+          break;
+        case 'd':
+          dptr = va_arg(ap, int);
+          if (dptr == 0) {
+            strcpy(dtos, "0");
+            k = 1;
+          }
+          else if (dptr == 0x80000000) {
+            strcpy(dtos, minint);
+            isneg = 1;
+            k = 10;
+          }
+          else {
+            k = 0;
+            if (dptr < 0) {
+              dptr = -dptr;
+              isneg = 1;
+            }
+            while (dptr) {
+              dtos[k++] = dptr % 10 + '0';
+              dptr /= 10;
+            }
+          }
+          if (isneg)
+            str[i++] = '-';
+          while (k)
+            str[i++] = dtos[--k];
+          j += 2;
+          break;
+        default:
+          panic("Not implemented");
+          break;
+      }
+    }
+  }
+  str[i] = '\0';
+  va_end(ap);
+  return i;
+}
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -14,10 +75,16 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *str, const char *format, ...) {
+  va_list ap;
+  va_start(ap, format);
+  return gprintf(2, str, format, ap);
+}
+/*
+int sprintf(char *str, const char *format, ...) {
   size_t i = 0, j = 0, k;
   int dptr;
   char *sptr;
-  char minint[15] = {'2', '1', '4', '7', '4', '8', '3', '6', '4', '8'};
+  char minint[15] = "2147483638";
   char dtos[15] = {0};
   bool isneg = 0;
   va_list ap;
@@ -74,6 +141,7 @@ int sprintf(char *str, const char *format, ...) {
   va_end(ap);
   return i;
 }
+*/
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   panic("Not implemented");
