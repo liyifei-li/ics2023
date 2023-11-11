7164e222809518325537f8075610a6a1be091d9c
tracer-ics2023 <tracer@njuics.org>
2023-11-11 19:27:16 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:27:16 up 20:52,  1 user,  load average: 0.09, 0.22, 0.34

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -170,6 +170,6 @@ void jalr_ftrace(vaddr_t curpc, vaddr_t dnpc, uint32_t instval, int rd, word_t i
   if (funccnt == 0) return;
   if (instval == 0x00008067)
     ret_ftrace(curpc, dnpc, ffname(curpc));
-  else if (rd == 1 || (rd == 0 && imm == 0))
+  else if (rd == 1)
     call_ftrace(curpc, dnpc, ffname(dnpc));
 }
