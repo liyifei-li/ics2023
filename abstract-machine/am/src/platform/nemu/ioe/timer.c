d8c9bee873c39b65fb5ba39e12869b8c4b923e0d
tracer-ics2023 <tracer@njuics.org>
2023-11-11 15:32:30 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:32:30 up 16:58,  1 user,  load average: 0.13, 0.16, 0.24

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,7 +5,8 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+//  uptime->us = 0;
+  uptime->us = (uint64_t)inl(rtc_port_base) + (uint64_t)inl(rtc_port_bae + 1) << 32;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
