aa8108facb69937797cb61e682ee0542c2300027
tracer-ics2023 <tracer@njuics.org>
2023-11-22 22:46:45 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:46:45 up  2:40,  1 user,  load average: 0.44, 0.51, 0.47

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -27,10 +27,10 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 //  int w = io_read(AM_GPU_CONFIG).width;
 //  int h = io_read(AM_GPU_CONFIG).height;
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (int i = 0; i < 100; i++)
-    printf("%x ", fb[i]);
-  printf("\n\n\n");
+//  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+//  for (int i = 0; i < 100; i++)
+//    printf("%x ", fb[i]);
+//  printf("\n\n\n");
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
