6067ef913b2a0d4af89c45a490f34a0305c0424c
tracer-ics2023 <tracer@njuics.org>
2023-11-22 21:02:09 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:02:09 up 55 min,  1 user,  load average: 0.49, 0.39, 0.43

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -89,9 +89,8 @@ static void load_elf() {
   Assert(fp, "Can not open '%s'", elf_file);
 
   fseek(fp, 0, SEEK_END);
-  long size = ftell(fp);
-printf("111111");
-  Log("The elf is %s, size = %ld", elf_file, size);
+  //long size = ftell(fp);
+  //Log("The elf is %s, size = %ld", elf_file, size);
 printf("111111");
   fseek(fp, 0, SEEK_SET);
 
