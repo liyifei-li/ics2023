a89a798e4ac83df454a95b744dc4028c7b691fd9
tracer-ics2023 <tracer@njuics.org>
2023-12-07 15:24:14 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:24:14 up 41 min,  1 user,  load average: 0.36, 0.32, 0.27

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,7 +4,8 @@ void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
-    printf("%d", EVENT_YIELD);
+  printf("%d", EVENT_YIELD);
+  Log("aaaaa\n\n\n");
   switch (a[0]) {
     case 1: printf("OMG its yield111\n"); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
