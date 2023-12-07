f9f658a714fb58ea6afbbbf90148d84f2f95d380
tracer-ics2023 <tracer@njuics.org>
2023-12-07 22:55:22 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:55:22 up  8:12,  1 user,  load average: 0.32, 0.30, 0.27

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,15 @@
 #include <common.h>
 #include "syscall.h"
+
+#ifndef CONFIG_STRACE
+#define CONFIG_STRACE
+#endif
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
   #ifdef CONFIG_STRACE
-    printf("Syscall NO.%u", a[0]);
+    printf("Syscall NO.%u\n", a[0]);
   #endif
   switch (a[0]) {
     case 0: halt(0); break;
