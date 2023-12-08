fc0b8d6a534b0d1a6745621bcba6d02b189f1d5a
tracer-ics2023 <tracer@njuics.org>
2023-12-08 13:59:53 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:59:53 up  5:53,  1 user,  load average: 0.46, 0.39, 0.36

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -15,26 +15,10 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case SYS_exit: halt(0); break;
     case SYS_yield: yield(); c->GPRx = 0; break;
-    case SYS_write:
-      do_syswrite(a[1], (char *)a[2], a[3]);
-      /*
-      if (a[1] == 1 || a[1] == 2) {
-        char *buf = (char *)a[2];
-        size_t count = (size_t)a[3];
-        for (int i = 0; i < count; i++) {
-          putch(*(buf + i));
-        }
-        c->GPRx = count;
-      }
-      else {
-        c->GPRx = -1;
-      }
-  */
-    break;
+    case SYS_write: c->GPRx = do_syswrite(a[1], (char *)a[2], a[3]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
-
 
 int do_syswrite(int fd, char *buf, size_t count) {
   if (fd == 1 || fd == 2) {
