48999c2530effc8b05a0d421b5e5ce976ffc15fe
tracer-ics2023 <tracer@njuics.org>
2023-12-08 13:59:05 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:59:05 up  5:52,  1 user,  load average: 0.58, 0.39, 0.36

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,8 @@
 #include <common.h>
 #include "syscall.h"
 
+int do_syswrite(int fd, char *buf, size_t count);
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -14,6 +16,8 @@ void do_syscall(Context *c) {
     case SYS_exit: halt(0); break;
     case SYS_yield: yield(); c->GPRx = 0; break;
     case SYS_write:
+      do_syswrite(a[1], (char *)a[2], a[3]);
+      /*
       if (a[1] == 1 || a[1] == 2) {
         char *buf = (char *)a[2];
         size_t count = (size_t)a[3];
@@ -22,7 +26,22 @@ void do_syscall(Context *c) {
         }
         c->GPRx = count;
       }
+      else {
+        c->GPRx = -1;
+      }
+  */
     break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+}
+
+
+int do_syswrite(int fd, char *buf, size_t count) {
+  if (fd == 1 || fd == 2) {
+    for (int i = 0; i < count; i++) {
+      putch(*(buf + i));
+    }
+    return count;
+  }
+  return -1;
 }
