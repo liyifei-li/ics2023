dcd759af3acbf3ef71812db847afada1dd16314e
tracer-ics2023 <tracer@njuics.org>
2023-12-08 13:45:29 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:45:29 up  5:39,  1 user,  load average: 0.16, 0.25, 0.30

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -11,6 +11,7 @@ Context* __am_irq_handle(Context *c) {
       case -1: ev.event = EVENT_YIELD; break;
       case 0: ev.event = EVENT_SYSCALL; break;
       case 1: ev.event = EVENT_SYSCALL; break;
+      case 4: ev.event = EVENT_SYSCALL; break;
       default: ev.event = EVENT_ERROR; break;
     }
     c = user_handler(ev, c);
