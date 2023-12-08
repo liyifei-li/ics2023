14046512ec08827e2f4341d2961008bc724b6f04
tracer-ics2023 <tracer@njuics.org>
2023-12-08 13:46:53 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:46:53 up  5:40,  1 user,  load average: 0.21, 0.26, 0.29

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -66,7 +66,7 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
-  _syscall_(SYS_write, (uintptr_t)fd, (uintptr_t)buf, (uintptr_t)count);
+  _syscall_(SYS_exit, (uintptr_t)fd, (uintptr_t)buf, (uintptr_t)count);
   return GPRx;
 }
 
