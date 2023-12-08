f9c3fd623869d8b6b845d46bd771f5bcb3b36420
tracer-ics2023 <tracer@njuics.org>
2023-12-08 10:21:01 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:21:01 up  2:14,  5 users,  load average: 0.37, 0.39, 0.30

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,7 +6,6 @@ static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD: printf("EVENT YIELD!\n"); break;
     case EVENT_SYSCALL:
-      printf("EVENT SYSCALL!\n");
       do_syscall(c);
       break;
     default: panic("Unhandled event ID = %d", e.event);
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,12 +4,20 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  #ifdef CONFIG_TRACE
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
+  #ifdef CONFIG_STRACE
     printf("Syscall NO.%u\n", a[0]);
   #endif
   switch (a[0]) {
-    case 0: halt(0); break;
-    case 1: yield(); c->GPRx = 0; break;
+    case SYS_exit: halt(0); break;
+    case SYS_yield: yield(); c->GPRx = 0; break;
+    case SYS_write:
+      if (a[1] == 1 || a[1] == 2) {
+
+      }
+    break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
