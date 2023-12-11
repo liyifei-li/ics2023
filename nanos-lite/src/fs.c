648036d0dca8f4cbe92cf356c45d047a8114fd89
tracer-ics2023 <tracer@njuics.org>
2023-12-11 22:06:32 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:06:32 up  7:27,  1 user,  load average: 2.14, 1.21, 0.64

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -62,6 +62,7 @@ size_t fs_write(int fd, const void *buf, size_t len) {
   size_t file_size = file_table[fd].size;
   size_t file_disk_offset = file_table[fd].disk_offset;
   size_t file_open_offset = file_table[fd].open_offset;
+    printf("fs_read: %d %d %d\n", file_size, file_disk_offset, file_open_offset);
   assert(file_open_offset + len <= file_size);
   ramdisk_write(buf, file_disk_offset + file_open_offset, len);
   file_table[fd].open_offset += len;
