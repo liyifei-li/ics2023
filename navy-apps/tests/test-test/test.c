6beeda9adc55d178d2407da59d34b2e727a99232
tracer-ics2023 <tracer@njuics.org>
2023-12-11 19:59:18 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:59:18 up  5:19,  1 user,  load average: 0.37, 0.30, 0.36

--- navy-apps/tests/test-test/test.c
+++ navy-apps/tests/test-test/test.c
@@ -5,11 +5,13 @@
 int main() {
   FILE *fp = fopen("/share/files/num", "r+");
   assert(0);
+  /*
   printf("Hello\n");
   printf("Bye\n");
   FILE *asdf = NULL;
   asdf = asdf;
   printf("Bye\n");
   assert(0);
+  */
   return 0;
 }
