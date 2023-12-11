b20aeaa2852be0f43f6b687eacea69952818023f
tracer-ics2023 <tracer@njuics.org>
2023-12-11 19:51:31 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:51:31 up  5:12,  1 user,  load average: 0.35, 0.34, 0.40

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -2,9 +2,8 @@
 #include <assert.h>
 
 int main() {
-  FILE *ffpp = NULL;
-  assert(ffpp);
   FILE *fp = fopen("/share/files/num", "r+");
+  assert(fp);
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
---
+++ navy-apps/tests/test-test/test.c
@@ -0,0 +1,8 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+  printf("Hello\n");
+  printf("Bye\n");
+  return 0;
+}
