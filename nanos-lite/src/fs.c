6102ada29077d2d66cb5d31ff419fc328b98b8e8
tracer-ics2023 <tracer@njuics.org>
2023-12-11 22:12:52 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:12:52 up  7:33,  1 user,  load average: 0.35, 0.56, 0.53

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -48,22 +48,20 @@ int fs_open(const char *pathname, int flags, int mode) {
 }
 
 size_t fs_read(int fd, void *buf, size_t len) {
-  size_t file_size = file_table[fd].size;
   size_t file_disk_offset = file_table[fd].disk_offset;
   size_t file_open_offset = file_table[fd].open_offset;
-  printf("fs_read: %d %d %d %d\n", file_size, file_disk_offset, file_open_offset, len);
-  assert(file_open_offset + len <= file_size);
+//  printf("fs_read: %d %d %d %d\n", file_size, file_disk_offset, file_open_offset, len);
+//  assert(file_open_offset + len <= file_size);
   ramdisk_read(buf, file_disk_offset + file_open_offset, len);
   file_table[fd].open_offset += len;
   return len;
 }
 
 size_t fs_write(int fd, const void *buf, size_t len) {
-  size_t file_size = file_table[fd].size;
   size_t file_disk_offset = file_table[fd].disk_offset;
   size_t file_open_offset = file_table[fd].open_offset;
-  printf("fs_write: %d %d %d %d\n", file_size, file_disk_offset, file_open_offset, len);
-  assert(file_open_offset + len <= file_size);
+//  printf("fs_write: %d %d %d %d\n", file_size, file_disk_offset, file_open_offset, len);
+//  assert(file_open_offset + len <= file_size);
   ramdisk_write(buf, file_disk_offset + file_open_offset, len);
   file_table[fd].open_offset += len;
   return len;
