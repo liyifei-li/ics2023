65a7d38e974ef761fe41bb02803617aa9f6a7e3f
tracer-ics2023 <tracer@njuics.org>
2023-12-12 22:52:52 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:52:52 up  3:28,  1 user,  load average: 0.45, 0.44, 0.38

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -29,7 +29,7 @@ void do_syscall(Context *c) {
     case SYS_lseek: c->GPRx = fs_lseek(a[1], a[2], a[3]); break;
     case SYS_brk: c->GPRx = sys_brk((void *)a[1]); break;
 //    case SYS_execve: c->GPRx = do_sysexecve((const char *)a[1], (char *)a[2], (char *)a[3]); break;
-//    case SYS_gettimeofday: c->GPRx = sys_gettimeofday(struct timeval *a[1], struct timeval *a[2]); break;
+    case SYS_gettimeofday: c->GPRx = sys_gettimeofday((struct timeval *)a[1], (struct timezone *)a[2]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
@@ -37,8 +37,7 @@ void do_syscall(Context *c) {
 int sys_brk(void *addr) {
   return 0;//OK for PA3
 }
-/*
+
 int sys_gettimeofday(struct timeval *tv, struct timezone *tz) {
   return 0;
 }
-*/
