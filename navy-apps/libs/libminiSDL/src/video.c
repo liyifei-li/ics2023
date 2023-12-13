38046bea9a15439b4de83b8813b0941bd732634f
tracer-ics2023 <tracer@njuics.org>
2023-12-13 22:01:21 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:01:21 up 1 day,  2:37,  1 user,  load average: 0.28, 0.34, 0.35

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -33,6 +33,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
     h = atoi(ptr);
     close(fd); 
   }
+  printf("%d %d %d %d\n", x, y, w, h);
   uint32_t BitsPerPixel = s->format->BitsPerPixel;
   int fd = open("/dev/fb", O_WRONLY);
   uint32_t *pos = s->pixels;
