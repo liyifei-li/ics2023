0841a0e9a674a1bc4173c338181f1a8de2317b69
tracer-ics2023 <tracer@njuics.org>
2023-12-13 13:45:49 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:45:49 up 18:21,  1 user,  load average: 0.41, 0.43, 0.38

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <sys/time.h>
 
-static int evtdev = -1;
+static int evtdev = 3;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
 
@@ -16,8 +16,7 @@ uint32_t NDL_GetTicks() {
 }
 
 int NDL_PollEvent(char *buf, int len) {
-  //int fd = fs_open("/dev/events", 0, 0);
-  return read(3, buf, len) != 0;
+  return read(evtdev, buf, len) != 0;
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
