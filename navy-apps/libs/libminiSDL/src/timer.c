2110be147c4b61eedb65f022247490bac8aa8f75
tracer-ics2023 <tracer@njuics.org>
2023-12-15 15:07:07 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:07:07 up 3 days,  3:43,  1 user,  load average: 0.91, 0.87, 0.89

--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -15,7 +15,7 @@ int SDL_RemoveTimer(SDL_TimerID id) {
 uint32_t SDL_GetTicks() {
   struct timeval tv;
   gettimeofday(&tv, NULL);
-  return tv.tv_sec;
+  return tv.tv_sec * 1000 + tv.tv_usec / 1000;
 }
 
 void SDL_Delay(uint32_t ms) {
--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -15,7 +15,7 @@ static int screen_w = 0, screen_h = 0;
 uint32_t NDL_GetTicks() {
   struct timeval tv;
   gettimeofday(&tv, NULL);
-  return tv.tv_sec * 1000000 + tv.tv_usec;
+  return tv.tv_sec * 1000 + tv.tv_usec / 1000;
 }
 
 int NDL_PollEvent(char *buf, int len) {
