14ddd68eefee2a6e0442b1dc711bac9cbb462dcb
tracer-ics2023 <tracer@njuics.org>
2023-12-15 19:27:30 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:27:30 up 3 days,  8:03,  1 user,  load average: 1.11, 1.06, 1.00

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -206,6 +206,7 @@ void SDL_SoftStretch(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
     /* The source rectangle and the destination rectangle
      * are of the same size. If that is the case, there
      * is no need to stretch, just copy. */
+    printf("%d %d %d %d\n", w, dstrect->w, h, dstrect->h);
     SDL_Rect rect;
     rect.x = x;
     rect.y = y;
