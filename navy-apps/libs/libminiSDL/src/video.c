4573d658bf2e7cc9a21687b6c96af8c9fa1825ae
tracer-ics2023 <tracer@njuics.org>
2023-12-15 21:57:20 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:57:20 up 3 days, 10:33,  1 user,  load average: 0.76, 0.84, 0.80

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -95,7 +95,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
       for (int j = 0; j < w; j++) {
         lseek(fd, 4 * (x + j + (i + y) * s->w), SEEK_SET);
         write(fd, &s->format->palette->colors[*(uint8_t *)pos], 4);
-        printf("%u", s->format->palette->colors[*(uint8_t *)pos]);
+        printf("%u", *(uint8_t *)pos);
         pos++;
       }
     }
