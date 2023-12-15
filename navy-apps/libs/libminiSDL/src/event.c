7d3f66517b7c62173999e82b0ff410446071cbe1
tracer-ics2023 <tracer@njuics.org>
2023-12-15 23:02:32 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:02:32 up 3 days, 11:38,  1 user,  load average: 1.34, 0.93, 0.74

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -50,9 +50,11 @@ int SDL_WaitEvent(SDL_Event *event) {
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
+  assert(0);
   return 0;
 }
 
 uint8_t* SDL_GetKeyState(int *numkeys) {
+  assert(0);
   return NULL;
 }
