5dff547d20533eefd06cb038ce33596f75a2d53a
tracer-ics2023 <tracer@njuics.org>
2023-12-15 10:17:23 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:17:23 up 2 days, 14:53,  1 user,  load average: 0.56, 0.63, 0.55

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -22,15 +22,14 @@ struct BitmapHeader {
 void* BMP_Load(const char *filename, int *width, int *height) {
   FILE *fp = fopen(filename, "r");
   if (!fp) return NULL;
-  printf("893\n");
 
   struct BitmapHeader hdr;
   assert(sizeof(hdr) == 54);
   assert(1 == fread(&hdr, sizeof(struct BitmapHeader), 1, fp));
 
+  printf("%d %d\n", hdr.bitcount, hdr.compression);
   if (hdr.bitcount != 24) return NULL;
   if (hdr.compression != 0) return NULL;
-  printf("114514\n");
   int w = hdr.width;
   int h = hdr.height;
   uint32_t *pixels = malloc(w * h * sizeof(uint32_t));
