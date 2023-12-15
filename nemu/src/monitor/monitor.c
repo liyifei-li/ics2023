9e680cd0560f364f1737d514105f4247a803fb23
tracer-ics2023 <tracer@njuics.org>
2023-12-15 21:18:11 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:18:11 up 3 days,  9:54,  1 user,  load average: 0.78, 0.60, 0.72

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -140,8 +140,8 @@ static void load_elf() {
     Log("%d", i);
       funclist[funccnt].addr = sym.st_value;
       funclist[funccnt].size = sym.st_size;
-    Log("%d", i);
       strcpy(funclist[funccnt].name, strtab + sym.st_name);
+    Log("%d", i);
       funccnt++;
     }
   }
