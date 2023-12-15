4aef81ab8525bb95e0947924abacfb9a88a3614b
tracer-ics2023 <tracer@njuics.org>
2023-12-15 10:05:21 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:05:21 up 2 days, 14:41,  1 user,  load average: 0.39, 0.46, 0.46

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -45,6 +45,7 @@ void* BMP_Load(const char *filename, int *width, int *height) {
       pixels[w * i + j] = (r << 16) | (g << 8) | b;
     }
   }
+  assert(pixels);
 
   fclose(fp);
   if (width) *width = w;
