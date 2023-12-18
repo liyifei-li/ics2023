fb78833c57bfd406f383d822e24dfd05724fcc68
tracer-ics2023 <tracer@njuics.org>
2023-12-18 21:52:03 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:52:03 up 23:32,  5 users,  load average: 0.88, 0.96, 0.99

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -51,7 +51,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
   */
   for (int i = 0; i < srcrect->h; i++) {
     pos = dst->pixels + BytesPerPixel * ((dstrect->x) + (i + dstrect->y) * dst->w);
-    memcpy(pos, buf + BytesPerPixel * (i * srcrect->w), BytesPerPixel * srcrect->w);
+    memcpy(buf + BytesPerPixel * (i * srcrect->w), pos, BytesPerPixel * srcrect->w);
   }
   if (free_srcrect) free(srcrect);
   if (free_dstrect) free(dstrect);
