8a9fff3c3a84985fea20293412929c1c56771453
tracer-ics2023 <tracer@njuics.org>
2024-01-10 21:47:22 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:47:22 up 13:53,  1 user,  load average: 0.18, 0.32, 0.48

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -31,40 +31,39 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   while (argv[argv_length] != NULL) argv_length++;
   int envp_length = 0;
   while (envp[envp_length] != NULL) envp_length++;
-  uintptr_t argv_ptr[argv_length + 1], envp_ptr[envp_length + 1];
-  uintptr_t cur = (uintptr_t)new_page(8) + 8 * PGSIZE - 1;
+  uintptr_t *argv_ptr[argv_length + 1], *envp_ptr[envp_length + 1];
+  void *cur = new_page(8) + 8 * PGSIZE - 1;
   size_t len;
-  argv_ptr[argv_length] = 0;
-  envp_ptr[envp_length] = 0;
+  argv_ptr[argv_length] = NULL;
+  envp_ptr[envp_length] = NULL;
   for (int i = 0; i < argv_length; i++) {
     len = strlen(argv[i]);
     cur -= len + 1;
     strcpy((char *)cur, argv[i]);
-    argv_ptr[i] = (uintptr_t)cur;
+    argv_ptr[i] = cur;
   }
-  argv_ptr[argv_length] = 0;
   for (int i = 0; i < envp_length; i++) {
     len = strlen(envp[i]);
     cur -= len + 1;
     strcpy((char *)cur, envp[i]);
-    envp_ptr[i] = (uintptr_t)cur;
+    envp_ptr[i] = cur;
   }
   envp_ptr[envp_length] = 0;
-  cur &= 0xfffffffc;
+  cur = (void *)((uintptr_t)cur & 0xfffffffc);
   for (int i = envp_length; i >= 0; i--) {
-    cur -= 4;//4 is the length of uintptr_t *
-    *(uintptr_t *)cur = envp_ptr[i];
+    cur -= 4;//4 is the length of void *
+    *(uintptr_t *)cur = (uintptr_t)envp_ptr[i];
   }
   for (int i = argv_length; i >= 0; i--) {
-    cur -= 4;//4 is the length of uintptr_t *
-    *(uintptr_t *)cur = argv_ptr[i];
+    cur -= 4;
+    *(uintptr_t *)cur = (uintptr_t)argv_ptr[i];
   }
   cur -= 4;
   *(uintptr_t *)cur = argv_length;
   void *entry = (void *)loader(pcb, filename);
   
   pcb->cp = ucontext(NULL, (Area) { pcb->stack, pcb + 1 }, entry);
-  pcb->cp->GPRx = cur;
+  pcb->cp->GPRx = (uintptr_t)cur;
 }
 
 void init_proc() {
