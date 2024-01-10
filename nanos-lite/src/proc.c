ecebcf78b499a51a6932fcc165eaa65388309557
tracer-ics2023 <tracer@njuics.org>
2024-01-10 19:11:28 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:11:28 up 11:17,  1 user,  load average: 0.31, 0.32, 0.43

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -38,20 +38,17 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   for (int i = 0; i < argv_length; i++) {
     len = strlen(argv[i]);
     cur -= len + 1;
-    printf("1");
     strncpy(cur, argv[i], len);
-    printf("2");
     argv_ptr[i] = (uintptr_t)cur;
-    printf("3");
   }
-    printf("%p\n", argv_ptr[1]);
+    printf("%c\n", argv_ptr[1]);
   for (int i = 0; i < envp_length; i++) {
     len = strlen(envp[i]);
     cur -= len + 1;
     strncpy(cur, envp[i], len);
     envp_ptr[i] = (uintptr_t)cur;
   }
-    printf("%s\n", envp_ptr[1]);
+    printf("%c\n", envp_ptr[1]);
   assert(0);
   void *entry = (void *)loader(pcb, filename);
   
