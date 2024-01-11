afb61bf0636efdb8284f620481a91bf99204e3f2
tracer-ics2023 <tracer@njuics.org>
2024-01-11 09:28:11 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:28:11 up 1 day,  1:34,  3 users,  load average: 0.27, 0.59, 0.67

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -47,8 +47,8 @@ int sys_brk(void *addr) {
 int sys_execve(const char *fname, char * const argv[], char *const envp[]) {
   printf("sys_execve: %s %s %s\n", fname, argv[0], envp[0]);
   context_uload(current, fname, argv, envp);
-  switch_boot_pcb();
   printf("sys_exceve: \n");
+  switch_boot_pcb();
   yield();
   return -1;//Sane??
 }
