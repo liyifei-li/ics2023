1a7cab44c01d36c387f1510774a7f4095651b83e
tracer-ics2023 <tracer@njuics.org>
2024-01-11 08:49:56 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:49:56 up 1 day, 55 min,  3 users,  load average: 0.64, 0.54, 0.55

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -30,10 +30,10 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   printf("%s hahaha\n", filename);
   int argv_length = 0;
   while (argv[argv_length] != NULL) argv_length++;
+  printf("%s hahaha\n", filename);
   int envp_length = 0;
   while (envp[envp_length] != NULL) envp_length++;
   uintptr_t *argv_ptr[argv_length + 1], *envp_ptr[envp_length + 1];
-  printf("%s hahaha\n", filename);
   void *cur = new_page(8) + 8 * PGSIZE - 1;
   size_t len;
   argv_ptr[argv_length] = NULL;
