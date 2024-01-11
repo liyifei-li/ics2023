5525d47b6275babdb46e0c5ef9d921494b84c518
tracer-ics2023 <tracer@njuics.org>
2024-01-11 11:14:26 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:14:26 up 1 day,  3:20,  4 users,  load average: 0.80, 0.55, 0.43

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -48,7 +48,7 @@ int sys_brk(void *addr) {
 }
 
 int sys_execve(const char *fname, char * const argv[], char *const envp[]) {
-//  printf("sys_execve: %s %s %s\n", fname, argv[0], envp[0]);
+  printf("sys_execve: %s\n", fname);
   if (fs_open(fname, 0, 0) == -1) return -2;
   context_uload(current, fname, argv, envp);
   //printf("sys_exceve: \n");
