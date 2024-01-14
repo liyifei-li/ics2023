8d9335a70ae072bb688f00c281fad014e258abe4
tracer-ics2023 <tracer@njuics.org>
2024-01-14 14:46:00 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:46:00 up 23:55,  5 users,  load average: 0.54, 0.31, 0.31

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -38,7 +38,7 @@ extern void __am_asm_trap(void);
 bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
-
+  printf("aaa\n");
   // register event handler
   user_handler = handler;
 
