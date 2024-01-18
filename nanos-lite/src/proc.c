da88a2dbde78b01dfd79208a541a637f87ef7182
tracer-ics2023 <tracer@njuics.org>
2024-01-18 21:03:33 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:03:33 up  6:37,  5 users,  load average: 0.77, 0.56, 0.48

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -71,8 +71,8 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 void init_proc() {
   char *argv[] = {"/bin/pal", NULL};
   char *envp[] = {NULL};
-//  context_kload(&pcb[0], hello_fun, (void *)1);
-  context_uload(&pcb[0], "/bin/dummy", argv, envp);
+  context_kload(&pcb[0], hello_fun, (void *)1);
+  context_uload(&pcb[0], "/bin/pal", argv, envp);
   switch_boot_pcb();
 
   Log("Initializing processes...");
@@ -84,10 +84,6 @@ void init_proc() {
 
 Context *schedule(Context *prev) {
   current->cp = prev;
-  /*
   current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
-  return current->cp;
-  */
-  current = &pcb[0];
   return current->cp;
 }
