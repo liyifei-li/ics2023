5147b924a00da4deb29c6a7292891627e2b55eb5
tracer-ics2023 <tracer@njuics.org>
2024-01-18 19:51:33 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:51:33 up  5:25,  5 users,  load average: 0.19, 0.48, 0.73

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -48,6 +48,7 @@ uintptr_t loader(PCB *pcb, const char *filename) {
       Elf_Addr vaddr = phdr[i].p_vaddr;
       fs_lseek(fd, offset, SEEK_SET);
       void *curpage = (void *)(vaddr & 0xfffff000);
+      printf("stuff area: %p - %p\n", vaddr, vaddr - filesz);
       printf("0 area: %p - %p\n", vaddr + filesz, vaddr + memsz);
       while ((uintptr_t)curpage < vaddr + memsz) {
         void *newpage = new_page(1);
