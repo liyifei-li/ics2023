785650844b6b4dafdb97a47ca322586a359b8f49
tracer-ics2023 <tracer@njuics.org>
2024-01-18 22:23:34 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:23:34 up  7:57,  5 users,  load average: 0.58, 0.53, 0.48

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -63,6 +63,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   cur -= 4;
   *(uintptr_t *)cur = argv_length;
   void *entry = (void *)loader(pcb, filename);
+  printf("hey!!! %p\n", pcb->max_brk);
   
   pcb->cp = ucontext(&pcb->as, (Area) { pcb->stack, pcb + 1 }, entry);
   pcb->cp->GPRx = (uintptr_t)cur;
