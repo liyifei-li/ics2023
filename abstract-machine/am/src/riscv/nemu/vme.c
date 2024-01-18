33c7800adcfe0b56ce2af3f468bbf62aa22b868f
tracer-ics2023 <tracer@njuics.org>
2024-01-18 22:02:50 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:02:50 up  7:36,  5 users,  load average: 0.44, 0.40, 0.45

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -62,7 +62,7 @@ void __am_get_cur_as(Context *c) {
 
 void __am_switch(Context *c) {
   if (vme_enable && c->pdir != NULL) {
-    printf("%p\n", c->pdir);
+    printf("am_switch: %p\n", c->pdir);
     set_satp(c->pdir);
   }
 }
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -86,6 +86,6 @@ Context *schedule(Context *prev) {
   // printf("%p %p\n", current, &pcb[0]);
   current->cp = prev;
   current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
-  // printf("curcp %p\n", current->cp);
+  printf("curcp %p\n", current->cp);
   return current->cp;
 }
