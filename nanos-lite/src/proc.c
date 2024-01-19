534279289e4947351f6b11ba485631112e78d38d
tracer-ics2023 <tracer@njuics.org>
2024-01-19 20:59:15 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:59:15 up 53 min,  5 users,  load average: 0.57, 0.53, 0.44

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -95,7 +95,7 @@ Context *schedule(Context *prev) {
   current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   // current = &pcb[0];
   // printf("&current: %p\n", &current); //0x8214a010
-  // printf("%s:\ncurrent: %p, current->max_brk: %p, &current->max_brk: %p, current->cp->pdir: %p\n", current == &pcb[0] ? "kernel" : "user", current, current->max_brk, &current->max_brk, current->cp->pdir);
+  printf("%s:\ncurrent: %p, current->max_brk: %p, &current->max_brk: %p, current->cp->pdir: %p\n", current == &pcb[0] ? "kernel" : "user", current, current->max_brk, &current->max_brk, current->cp->pdir);
   // printf("hello_fun:%p\n", (void *)hello_fun);
   assert(current == &pcb[1] || current->cp->pdir == NULL);
   return current->cp;
