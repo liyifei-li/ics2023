9c58d2093a49dc4b5a29304a11da880a97c32d83
tracer-ics2023 <tracer@njuics.org>
2024-01-19 12:37:08 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:37:08 up 22:10,  5 users,  load average: 0.59, 0.50, 0.62

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -33,7 +33,7 @@ int mm_brk(uintptr_t brk) {
     return 0;
   }
   void *curpage = (void *)(((current->max_brk + 0xfff) & 0xfffff000));
-  while ((uintptr_t)curpage <= brk) {
+  while ((uintptr_t)curpage < brk) {
     printf("curpage: %p\n", curpage);
     void *newpage = new_page(1);
     map(&current->as, curpage, newpage, 0);
