86d3c5419c15e3177127b6abc8d906f773cdacd0
tracer-ics2023 <tracer@njuics.org>
2024-01-19 13:39:38 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:39:38 up 23:13,  5 users,  load average: 0.63, 0.45, 0.40

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -27,11 +27,12 @@
 #define OFFSET(n) ((n) & 0xfff)
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-  assert(vaddr < 0x78000000 || vaddr >= 0x80000000);
-  assert(vaddr < 0x7c000000 || vaddr >= 0x80000000);
-  assert(vaddr < 0x7e000000 || vaddr >= 0x80000000);
-  assert(vaddr < 0x7f000000 || vaddr >= 0x80000000);
-  assert(vaddr < 0x70000000 || vaddr >= 0x80000000);
+  assert(vaddr < 0x70000000 || vaddr >= 0x78000000);
+  assert(vaddr < 0x78000000 || vaddr >= 0x7c000000);
+  assert(vaddr < 0x7c000000 || vaddr >= 0x7e000000);
+  assert(vaddr < 0x7e000000 || vaddr >= 0x7f000000);
+  assert(vaddr < 0x7f000000 || vaddr >= 0x7ff00000);
+  assert(vaddr < 0x7ff00000 || vaddr >= 0x80000000);
   vaddr_t PTE1_ADDR = (cpu.satp << 12) + PTESIZE * VPN1(vaddr);
   PTE PTE1 = paddr_read(PTE1_ADDR, PTESIZE);
   assert(PTE1 & PTE_V);
