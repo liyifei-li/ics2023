0c7bfa29b8fea47d5f0fddb7cc1b0beea29125af
tracer-ics2023 <tracer@njuics.org>
2024-01-19 15:20:22 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:20:22 up 1 day, 54 min,  5 users,  load average: 0.54, 0.30, 0.26

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -36,7 +36,7 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
 //  assert(vaddr < 0x7fff8000 || vaddr >= 0x80000000);
   vaddr_t PTE1_ADDR = (cpu.satp << 12) + PTESIZE * VPN1(vaddr);
   PTE PTE1 = paddr_read(PTE1_ADDR, PTESIZE);
-//  if (PTE1 == 0) assert(vaddr < 0x7fff8000 || vaddr >= 0x80000000);
+  if (PTE1 == 0) assert(vaddr < 0x7fff8000 || vaddr >= 0x80000000);
   assert(PTE1);
   assert(PTE1 & PTE_V);
   vaddr_t PTE0_ADDR = PTEPAGE(PTE1) + PTESIZE * VPN0(vaddr);
