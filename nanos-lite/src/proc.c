3da68e4b5b4afc99223ed0ee8e0f34ca00a817e2
tracer-ics2023 <tracer@njuics.org>
2024-01-20 08:10:32 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:10:32 up 12:05,  5 users,  load average: 0.82, 0.64, 0.70

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -16,6 +16,7 @@ void hello_fun(void *arg) {
   int j = 1;
   while (1) {
     Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
+    j++;
     yield();
   }
 }
