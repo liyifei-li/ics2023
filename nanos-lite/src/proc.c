6f2df45c658227220267aedcd4153c80b1dcd14c
tracer-ics2023 <tracer@njuics.org>
2024-01-20 16:59:59 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:59:59 up 20:54,  5 users,  load average: 0.41, 0.51, 0.45

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -92,8 +92,15 @@ void init_proc() {
 }
 
 Context *schedule(Context *prev) {
+  static unsigned char cnt = 0;
   current->cp = prev;
-  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  if (cnt == 0) {
+    current = &pcb[0];
+  }
+  else {
+    current = &pcb[1];
+  }
+  cnt++;
   // current = &pcb[1];
   // printf("&current: %p\n", &current); //0x8214a010
   // printf("%s: current: %p, current->cp->mstatus: %p\n", current == &pcb[0] ? "kernel" : "user", current, current->cp->mstatus);
