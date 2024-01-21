5492141a357b532a6d1ce55479a5b538b990e418
tracer-ics2023 <tracer@njuics.org>
2024-01-21 20:19:54 +0800

    >  compile NEMU 221900500 李亦非 Linux e7cf09 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:19:54 up 1 day, 9 min,  5 users,  load average: 0.23, 0.27, 0.32

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -10,7 +10,6 @@ void __am_switch(Context *c);
 
 Context* __am_irq_handle(Context *c) {
   __am_get_cur_as(c);
-  printf("%d\n", c->mcause);
   if (user_handler) { //do_event
     Event ev = {0};
     switch (c->mcause) {
